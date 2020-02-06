f58ca91808f5534b4beba63803e59963e8992d56
Edvin <gammelsaeterg@gmail.com>
2020-02-06 16:25:44 +0100

    More balancing

--- Source/DawnOfSolaris/DawnOfSolaris.cpp
+++ Source/DawnOfSolaris/DawnOfSolaris.cpp
@@ -38,7 +38,7 @@ float calculateKnockbackLength(float inMultiplier)
 		float inMultiplierInPercent = (inMultiplier - flinchAndKnockbackMinLimit) / (launchMinLimit - flinchAndKnockbackMinLimit);
 		
 		float minKnockbackValue = 100.f;
-		float maxKnockbackValue = 4000.f;
+		float maxKnockbackValue = 2000.f;
 		float knockbackDifferene = maxKnockbackValue - minKnockbackValue;
 
 		return minKnockbackValue + (inMultiplierInPercent * knockbackDifferene);
